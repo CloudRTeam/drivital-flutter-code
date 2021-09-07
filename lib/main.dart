3dbf85ae3dee853be67a57214be2b5aca97ed476
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-09-07 21:23:36 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // this is a test
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
